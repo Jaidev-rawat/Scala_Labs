9b5353f3090d5d6781bb6c7cfe789dea3f2e0502
Jaidev-rawat <jaidevrawat2005@gmail.com>
2021-08-07 15:06:22 +0530

    lab_3

---
+++ lab 3/kewor.scala
@@ -0,0 +1,28 @@
+
+
+object kewor {
+ // object keyword to define a class
+  def main(args: Array[String])  {
+    // main denotes definition of main fucntion
+    // String is a predefined data type to hold alphanumeric objects
+    var VAR = scala.io.StdIn.readLine();
+    // Mutable objects
+    // readline to denote String input from standard input;
+    var VAL = true
+   val k =4
+    while (VAL) {
+      // while to run a loop
+      print("Printing something here");
+      VAL = false;
+      // false is a boolean value
+    }
+  print("_branch")
+    // Used to print on console 
+println()
+// used to print on console and to add next line escape sequence at last
+printf("Age = %d", 24)
+// used to print on console using format specifiers
+ //return 0
+// used for returning the value
+  }
+}
